d84b3c28cf6d5f6305ce8f242c1e9dd0f1e0c0bd
roshan1662 <142892748+roshan1662@users.noreply.github.com>
2023-08-24 16:24:26 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,70 +1,13 @@
-from django.urls import path
-from app import views
-from django.contrib.auth import views as auth_views
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-
-
 urlpatterns = [
-    path('', views.home,name='home'),
-    path('product-detail/<int:pk>', views.product_detail, name='product-detail'),
-    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
-    path('cart/', views.show_cart, name='showcart'),
-
-
-    path('pluscart/', views.plus_cart),
-    path('minuscart/', views.minus_cart),
-    path('removecart/', views.remove_cart),
-
-
-
-
-    path('buy/', views.buy_now, name='buy-now'),
-    path('profile/', views.profile, name='profile'),
-    path('address/', views.address, name='address'),
-    path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
-    path('mobile/', views.mobile, name='mobile'),
-    path('mobile/<slug:data>/', views.mobile, name='mobiledata'),
-    path('laptop/', views.laptop, name='laptop'),
-    path('laptop/<slug:data>/', views.laptop, name='laptopdata'),
-    path('topwear/', views.topwear, name='topwear'),
-    path('bottomwear/', views.bottomwear, name='bottomwear'),
-    path('login/', views.loginpage, name='login'),
-    path('logout/', views.logoutpage, name='logout'),
-    path('registration/', views.customerregistration, name='customerregistration'),
-    path('checkout/', views.checkout, name='checkout'),
-
-    path('paymentdone/', views.payment_done, name='paymentdone'),
-
-
-# template_name="app/password_reset.html"
-# template_name="app/password_reset_done.html"
-# template_name="app/password_reset_confirm.html"
-# template_name="app/password_reset_complete.html" 
-
-    path(
-        "reset_password/",
-        auth_views.PasswordResetView.as_view(template_name="app/password_reset_form.html"), name="password_reset"
-    ),
-    path(
-        "password_reset_done/",
-        auth_views.PasswordResetDoneView.as_view(template_name="app/password_reset_done.html"),name='password_reset_done'
-    ),
-    
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(  template_name="app/password_reset_confirm.html"),name='password_reset_confirm'
-    ),
-    
-    path(
-        "password_reset_complete/",
-        auth_views.PasswordResetCompleteView.as_view(template_name="app/password_reset_complete.html" ),name='password_reset_complete'
-    ),
+    path('admin/', admin.site.urls),
+    path('', include('app.urls'))
 ]
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
